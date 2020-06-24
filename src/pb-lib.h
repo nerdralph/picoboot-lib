9a266dc448ad54fbdde40f4b83fcc9322dad00da
Nerd Ralph <ralphdoncaster@gmail.com>
2020-06-24 10:54:49 -0300

    v 0.2

--- src/pb-lib.h
+++ src/pb-lib.h
@@ -1,1 +1,9 @@
-/* dummy include for Arduino library spec */
+// picoboot-lib include
+
+__attribute__ ((naked, used))
+__attribute__ ((section (".init1")))
+void pb_lib(void)
+{
+    // picoboot-lib starts at the last 256 bytes of flash
+    asm("rcall __vectors - 256");
+}
