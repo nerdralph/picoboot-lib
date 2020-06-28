37e535b33acea1eb2636e36da62d4dc753fe04db
Nerd Ralph <ralphdoncaster@gmail.com>
2020-06-28 14:35:14 -0300

    v0.5.0

--- src/pb-lib.h
+++ src/pb-lib.h
@@ -1,5 +1,6 @@
 // picoboot-lib include
 
+/*
 __attribute__ ((naked, used))
 __attribute__ ((section (".init1")))
 void pb_lib(void)
@@ -7,6 +8,7 @@ void pb_lib(void)
     // picoboot-lib starts at the last 256 bytes of flash
     asm("rcall __vectors - 256");
 }
+*/
 
 // TxByte wrapper
 inline void pb_txbyte(char c)
